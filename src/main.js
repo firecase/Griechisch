ad3b55c729c19723a2cbf79c8c45ae8f5cd7f5a1
firecase <timohte@gmx.ch>
2022-10-17 00:31:43 +0200

    work on

--- src/main.js
+++ src/main.js
@@ -1,37 +1,47 @@
 class Buchstabe {
-    constructor(grossbuchstabe, kleinbuchstabe, aussprache, lateinischerbuchstabe) {
+    constructor (grossbuchstabe, kleinbuchstabe, aussprache, lateinischerbuchstabe, id) {
         this.Grossbuchstabe = grossbuchstabe;
         this.Kleinbuchstabe = kleinbuchstabe;
         this.Aussprache = aussprache;
         this.Lateinischerbuchstabe = lateinischerbuchstabe;
+        this.ID = id;
+    }
+}
+
+class rBuchstabe {
+    constructor (griechischerbuchstabe, aussprache, lateinischerbuchstabe, id) {
+        this.Griechischerbuchstabe = griechischerbuchstabe;
+        this.Aussprache = aussprache;
+        this.Lateinischerbuchstabe = lateinischerbuchstabe;
+        this.ID = id;
     }
 }
 
 var Alphabet = [
-    new Buchstabe("Α", "α", "Alpha", "A"),
-    new Buchstabe("Β", "β", "Beta", "B"),
-    new Buchstabe("Γ", "γ", "Gamma", "G"),
-    new Buchstabe("Δ", "δ", "Delta", "D"),
-    new Buchstabe("Ε", "ε", "Epsilon", "E"),
-    new Buchstabe("Ζ", "ζ", "Zeta", "Z"),
-    new Buchstabe("Η", "η", "Eta", "Ä"),
-    new Buchstabe("Θ", "θ", "Theta", "TH"),
-    new Buchstabe("Ι", "ι", "Iota", "I"),
-    new Buchstabe("Κ", "κ", "Kappa", "K"),
-    new Buchstabe("Λ", "λ", "Lamda", "L"),
-    new Buchstabe("Μ", "μ", "Mü", "M"),
-    new Buchstabe("Ν", "ν", "Ny", "N"),
-    new Buchstabe("Ξ", "ξ", "Xi", "X"),
-    new Buchstabe("Ο", "ο", "Omikron", "O (kurz)"),
-    new Buchstabe("Π", "π", "Pi", "P"),
-    new Buchstabe("Ρ", "ρ", "Rho", "R"),
-    new Buchstabe("Σ", "σ", "Sigma", "S"),
-    new Buchstabe("Τ", "τ", "Tau", "T"),
-    new Buchstabe("Υ", "υ", "Ypsilon", "Y"),
-    new Buchstabe("Φ", "φ", "Phi", "F"),
-    new Buchstabe("Χ", "χ", "Chi", "CH"),
-    new Buchstabe("Ψ", "ψ", "Psi", "PS"),
-    new Buchstabe("Ω", "ω", "Omega", "O (lang)")
+    new Buchstabe("Α", "α", "Alpha", "A", "1"),
+    new Buchstabe("Β", "β", "Beta", "B", "2"),
+    new Buchstabe("Γ", "γ", "Gamma", "G", "3"),
+    new Buchstabe("Δ", "δ", "Delta", "D", "4"),
+    new Buchstabe("Ε", "ε", "Epsilon", "E", "5"),
+    new Buchstabe("Ζ", "ζ", "Zeta", "Z", "6"),
+    new Buchstabe("Η", "η", "Eta", "Ä", "7"),
+    new Buchstabe("Θ", "θ", "Theta", "TH", "8"),
+    new Buchstabe("Ι", "ι", "Iota", "I", "9"),
+    new Buchstabe("Κ", "κ", "Kappa", "K", "10"),
+    new Buchstabe("Λ", "λ", "Lamda", "L", "11"),
+    new Buchstabe("Μ", "μ", "Mü", "M", "12"),
+    new Buchstabe("Ν", "ν", "Ny", "N", "13"),
+    new Buchstabe("Ξ", "ξ", "Xi", "X", "14"),
+    new Buchstabe("Ο", "ο", "Omikron", "O (kurz)", "15"),
+    new Buchstabe("Π", "π", "Pi", "P", "16"),
+    new Buchstabe("Ρ", "ρ", "Rho", "R", "17"),
+    new Buchstabe("Σ", "σ", "Sigma", "S", "18"),
+    new Buchstabe("Τ", "τ", "Tau", "T", "19"),
+    new Buchstabe("Υ", "υ", "Ypsilon", "Y", "20"),
+    new Buchstabe("Φ", "φ", "Phi", "F", "21"),
+    new Buchstabe("Χ", "χ", "Chi", "CH", "22"),
+    new Buchstabe("Ψ", "ψ", "Psi", "PS", "23"),
+    new Buchstabe("Ω", "ω", "Omega", "O (lang)", "24")
 ];
 
 function getRandomInt(max) {
@@ -63,6 +73,9 @@ function theme() {
 }
 
 function table() {
+    var randomAlphabet = [];
+    var randomBuchstabe;
+    var randomAlphabetlength;
     var content = document.querySelector('#content');
     var table = document.createElement('table');
     var tbody = document.createElement('tbody');
@@ -93,24 +106,42 @@ function table() {
 
     for (var a = 0; a < Alphabet.length; a++) {
         var tr = document.createElement('tr');
-        var random_greek_letter = getRandomInt(2);
-        var random_row = getRandomInt(Alphabet.length);
+        var random_uppercase_lowercase = getRandomInt(2);
+        var random_letter = getRandomInt(Alphabet.length);
         var content_one;
-        var content_two = document.createTextNode(Alphabet[random_row].Aussprache);
-        var content_three = document.createTextNode(Alphabet[random_row].Lateinischerbuchstabe);
+        var content_two = document.createTextNode(Alphabet[random_letter].Aussprache);
+        var content_three = document.createTextNode(Alphabet[random_letter].Lateinischerbuchstabe);
 
-        if (random_greek_letter === 0) {
-            content_one = document.createTextNode(Alphabet[random_row].Grossbuchstabe);
+        if (random_uppercase_lowercase === 0) {
+            content_one = document.createTextNode(Alphabet[random_letter].Grossbuchstabe);
         } else {
-            content_one = document.createTextNode(Alphabet[random_row].Kleinbuchstabe);
+            content_one = document.createTextNode(Alphabet[random_letter].Kleinbuchstabe);
         }
 
-        for (var b = 0; b < 3; b++) {
+        if (randomAlphabet.length === 0) {
+            randomBuchstabe = new rBuchstabe (content_one, content_two, content_three, Alphabet[random_letter].ID);
+            randomAlphabet.push(randomBuchstabe);
+        }
+
+        randomAlphabetlength = randomAlphabet.length;
+        console.log(randomAlphabetlength);
+
+        for (var b = 0; b < randomAlphabetlength; b++) {
+            if (randomAlphabet[b].ID === Alphabet[random_letter].ID) {
+                console.log('wow');
+            } /*else {
+                randomBuchstabe = new rBuchstabe (content_one, content_two, content_three, Alphabet[random_letter].ID);
+                randomAlphabet.push(randomBuchstabe);
+                console.log(randomAlphabet);
+            }*/
+        }
+
+        for (var c = 0; c < 3; c++) {
             var td = document.createElement('td');
 
-            if (b === 0) {
+            if (c === 0) {
                 td.appendChild(content_one);
-            } else if (b === 1) {
+            } else if (c === 1) {
                 td.appendChild(content_two);
             } else {
                 td.appendChild(content_three);
